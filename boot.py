b48115ef4f0915fdcdaf5cb36e7164a0786e238c
Jan Haering <jan.haering@heureka.cz>
2018-04-23 15:50:14 +0200

    Clean up

--- boot.py
+++ boot.py
@@ -4,8 +4,8 @@ import network
 import time
 import config
 
-pin25 = machine.Pin(25, machine.Pin.OUT)
-pin25.value(0)
+builtin_led = machine.Pin(25, machine.Pin.OUT)
+builtin_led.value(0)
 
 nic = network.WLAN(network.STA_IF)
 nic.active(True)
@@ -13,5 +13,4 @@ nic.connect(config.wifi_essid, config.wifi_key)
 while not nic.isconnected():
     time.sleep(1)
 
-pin25.value(nic.isconnected())
-
+builtin_led.value(nic.isconnected())
