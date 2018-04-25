0bee0df2f981ac8efe6a5f857ebf21622fbd79e9
Jan Haering <jan.haering@heureka.cz>
2018-04-25 10:25:37 +0200

    FTP server

--- boot.py
+++ boot.py
@@ -2,9 +2,13 @@
 import machine
 import oled
 import wifi
+from lib.ftp import ftpserver
 
 builtin_led = machine.Pin(25, machine.Pin.OUT)
 builtin_led.value(0)
 
 oled.setup()
 wifi.setup(builtin_led)
+
+ftpserver = ftpserver()
+ftpserver.start_thread()
